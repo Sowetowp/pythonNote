e18c06bb7550c9516d1ec1e1a64175e75f2e4b13
Abdulmalik <ayodejiamzat@gmail.com>
2023-12-23 14:23:18 +0100

    d

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ x = ["a", 'b', 'c']
 for i in x:
     print(i)
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango", "pawpaw"]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango", "pawpa"]
 newlist = []
 
 for x in fruits:
