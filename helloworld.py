4de2e97523524da5e14b9cf8c515295a546851b6
Abdulmalik <ayodejiamzat@gmail.com>
2025-01-21 10:35:26 +0100

    f

--- helloworld.py
+++ helloworld.py
@@ -3,18 +3,25 @@ x = ["a", 'b', 'c']
 for i in x:
     print(i)
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango", "paw"]
-newlist = []
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango", "paw"]
+# newlist = []
 
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
 
-print(newlist)
+# print(newlist)
 
-#  list comprehension 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# #  list comprehension 
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-newlist = [x for x in fruits if "a" in x]
+# newlist = [x for x in fruits if "a" in x]
 
-print(newlist)
+# print(newlist)
+
+
+even = [x for x in range(1,50) if x % 2 == 0]
+print(even)
+
+odd = [x for x in range(50, 1, -1) if x % 2 != 0]
+print(odd)
---
+++ index.py
@@ -0,0 +1,12 @@
+x = 3
+y= float(3.8)
+print(x, y)
+
+print(type(y))
+import random
+print(random.randint(1, 6))
+x = "malik"
+print(x[1])
+for u in "malik":
+    print(u)
+print(len(x))
